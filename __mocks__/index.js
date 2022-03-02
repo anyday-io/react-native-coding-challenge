ce2a48fe61662f831e26a8fbed8068c0552a3b7a
Juno <>
2022-03-03 00:14:14 +0700

    ⚡

---
+++ __mocks__/index.js
@@ -0,0 +1,16 @@
+jest.mock('@react-navigation/native', () => ({
+  NavigationContainer: jest.fn().mockReturnValue(function NavigationContainer(props) {
+    return null
+  }),
+  createSwitchNavigator: jest.fn(),
+  createDrawerNavigator: jest.fn(),
+  StackActions: {
+    push: jest.fn().mockImplementation((x) => ({ ...x, type: 'Navigation/PUSH' })),
+    replace: jest.fn().mockImplementation((x) => ({ ...x, type: 'Navigation/REPLACE' })),
+    reset: jest.fn(),
+  },
+  NavigationActions: {
+    navigate: jest.fn().mockImplementation((x) => x),
+  },
+  DrawerItems: () => 'DrawerItems',
+}))
